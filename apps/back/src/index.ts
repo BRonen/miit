1f1aacf03a183ae1398107fb522cc09f83a79b90
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-22 22:25:49 -0300

    style: formatting

--- apps/back/src/index.ts
+++ apps/back/src/index.ts
@@ -1,15 +1,15 @@
-import Koa from 'koa';
+import Koa from "koa";
 
 const app = new Koa();
 
 const a: number = 2;
 
-console.log(a)
+console.log(a);
 
 app.use((ctx) => {
-    ctx.body = 'hello world w';
+  ctx.body = "hello world w";
 });
 
 app.listen(9000, () => {
-    console.log('listening at http://0.0.0.0:9000/')
-})
+  console.log("listening at http://0.0.0.0:9000/");
+});
