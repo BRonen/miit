37911d53834f92342c3a0ff3e1b625e7c9f97920
Brenno Rodrigues <brennopereira6@gmail.com>
2024-01-26 04:43:47 -0300

    feat(api): adding create new user mutation

--- apps/api/src/users/users.mutation.ts
+++ apps/api/src/users/users.mutation.ts
@@ -1,13 +1,24 @@
 import { GraphQLFieldConfig, GraphQLString } from "graphql";
+import { userModel } from "../models/user.model";
+import { UserType } from "./users.types";
 
-export const usersMutation: GraphQLFieldConfig<unknown, unknown> = {
-    type: GraphQLString,
-    description: 'Sample query which returns string!',
+export type createUserMutationArgs = {
+    username: string
+    password: string
+};
+
+export const createUserMutation: GraphQLFieldConfig<unknown, unknown, createUserMutationArgs> = {
+    type: UserType,
+    description: 'Create new user mutation',
     args: {
-        awa: { type: GraphQLString }
+        username: { type: GraphQLString },
+        password: { type: GraphQLString },
     },
-    resolve: (_, { awa }: { awa: string }) => {
-        console.log({awa})
-        return 'Hello from createUser';
+    resolve: async (_, { username, password }) => {
+        console.log({ username, password });
+
+        const user = await userModel.create({ username, password });
+
+        return user;
     }
 };
